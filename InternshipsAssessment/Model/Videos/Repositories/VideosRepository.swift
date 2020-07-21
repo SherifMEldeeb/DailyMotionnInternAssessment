74de314cf6c11211c03b1b63caa52f313d64ce9b
SherifMEldeeb <sherif.eldeep1@gmail.com>
2020-07-21 11:51:00 +0200

    Finish VideosRepository

--- InternshipsAssessment/Model/Videos/Repositories/VideosRepository.swift
+++ InternshipsAssessment/Model/Videos/Repositories/VideosRepository.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+protocol VideosRepositoryProtocol {
+    func getMostRecentVideos(completion: @escaping (Result<Videos, Error>) -> Void)
+}
+
+class VideosRepository: VideosRepositoryProtocol {
+    private var isConnected: Bool
+    private let remoteRepo: VideosRemoteRepositoryProtocol
+    
+    init(remoteVideosRepositories: VideosRemoteRepositoryProtocol, isConnected: Bool = true) {
+        self.remoteRepo = remoteVideosRepositories
+        self.isConnected = isConnected
+    }
+    
+    func getMostRecentVideos(completion: @escaping (Result<Videos, Error>) -> Void) {
+        if isConnected {
+            remoteRepo.getMostRecentVideos { (result) in
+                switch result {
+                case  .success(let vids):
+                    completion(.success(vids))
+                case .failure(let err):
+                    completion(.failure(err))
+                }
+            }
+        }else {
+            //getCachedVideos
+            completion(.failure(NSError(domain: "Device is offline.", code: 999, userInfo: [:])))
+        }
+    }
+    
+}
